ba6fe6586a953b019915bdcb56cccfff090452cd
besedina <32320781+TomaBesedina@users.noreply.github.com>
2018-02-07 11:28:38 +0300

    Update InteractiveBlock.js

--- datavis/hw2/table/InteractiveBlock.js
+++ datavis/hw2/table/InteractiveBlock.js
@@ -1,4 +1,4 @@
-import {d3} from '../../libraries'
+import {d3} from 'https://github.com/TomaBesedina/Besedina.github.io/tree/master/datavis/hw2'
 
 
 const INTERACTIVE_BLOCK_LABEL_ID = "interactive_block_label";
