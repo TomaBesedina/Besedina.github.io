849dd513eba94b5141176ab5c45a96c5a40d0fa2
besedina <32320781+TomaBesedina@users.noreply.github.com>
2018-02-07 11:29:03 +0300

    Update Table.js

--- datavis/hw2/table/Table.js
+++ datavis/hw2/table/Table.js
@@ -1,4 +1,4 @@
-import {d3} from '../../libraries'
+import {d3} from 'https://github.com/TomaBesedina/Besedina.github.io/tree/master/datavis/hw2'
 import ValueFormatter from "./ValueFormatter";
 
 const TABLE_HEADER_ID = "table_header";
